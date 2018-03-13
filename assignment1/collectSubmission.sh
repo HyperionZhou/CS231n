386e2836ddcd7269453b930d873a66d143be66f7
周基源 <HypertasyZOY@zhoujiyuandeMacBook-Pro.local>
2018-03-13 22:04:46 +1030

    add current code

---
+++ assignment1/collectSubmission.sh
@@ -0,0 +1,3 @@
+rm -f assignment1.zip 
+zip -r assignment1.zip . -x "*.git*" "*cs231n/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" ".env/*"
+# mei shen me luan yong
